f8122ed0be86b82aa59de0247dfa3b54f0fa4115
OptiTradeAi <diegobinox88@gmail.com>
2025-05-09 14:26:04 -0300

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,1 +1,24 @@
-# configurações reais do usuário
+USER_CONFIG = {
+    "telegram_token": "7605762525:AAFHQWncC5Nf4cdGkvlBkwwvyShKwZxByLY",
+    "telegram_chat_id": "-1002695222621",
+    "horario_envio_sinais": {
+        "usar_sessoes": False,
+        "sessoes": [
+            {
+                "inicio": "09:00",
+                "fim": "12:00",
+                "max_sinais": 10
+            },
+            {
+                "inicio": "14:00",
+                "fim": "17:00",
+                "max_sinais": 10
+            }
+        ]
+    },
+    "limites": {
+        "usar_limites": True,
+        "stop_win": 3,
+        "stop_loss": 3
+    }
+}
